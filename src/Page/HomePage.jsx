6bd655a1c41f993d4e763eddb61de5f006d84f3d
Abhishek <abhisingh24082002@gmail.com>
2023-02-13 20:27:32 +0530

    today's commit

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <div id='home'>
       <img className='home-img' src="https://media.licdn.com/dms/image/D4D03AQG7umBwwZ3c8g/profile-displayphoto-shrink_200_200/0/1676298979077?e=1681948800&v=beta&t=5SAWBWjuEKvXZ_tLEH0uEaPwy8pKC-e4P5WIB2MmzWk" alt="" />
       <h3 id='user-detail-name'>Name</h3>
-      <h2 id='user-detail-intro'>My name is Abhishek Kumar Singh</h2>
+      <h2 id='user-detail-intro'>Aspiring MERN Developer</h2>
     </div>
   )
 }
